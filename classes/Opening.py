d501e44f2891f4e072bf300376433274538f93bc
jhealey <jjheals@gmail.com>
2023-11-06 11:53:04 -0500

    Renamed notebook & changed attrs for Opening

--- classes/Opening.py
+++ classes/Opening.py
@@ -1,17 +1,26 @@
 class Opening: 
     
+    links:dict
     wiki_link:str
     opening_name:str
     
-    def __init__(self, opening_name:str, wiki_link:str):
-        self.wiki_link = wiki_link
+    def __init__(self, opening_name:str, wiki_link:str, chess_com_link:str):
         self.opening_name = opening_name
-    
+        self.links = {
+                      'wikipedia': wiki_link,
+                      'chess.com': chess_com_link
+                    }
+        
     def to_dict(self) -> dict[str,str]: 
-        return {'opening-name': self.opening_name, 'wiki-link': self.wiki_link}
+        return {'opening-name': self.opening_name, 'links': self.links}
     
     
     def to_string(self) -> str: 
-        return f"Name: {self.opening_name}\nWikipedia: {self.wiki_link}"
+        s = f"Name: {self.opening_name}\n"
+        s += "Links: \n"
+
+        for s,l in self.links.items(): s += f"\t{s} - {l}\n"
+        
+        return s
     
     
