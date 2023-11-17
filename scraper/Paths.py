9a7adea23a5985d35561580ea5a9adb718b21fa1
akerekon <89589162+akerekon@users.noreply.github.com>
2023-11-17 12:06:01 -0500

    Fresh slate for website

--- scraper/Paths.py
+++
@@ -1,6 +0,0 @@
-class Paths: 
-    LICHESS_DATASET = 'data/kaggle-lichess.csv'
-    OPENINGS_JSON = 'data/openings.json'
-    INDEX_JSON = 'data/index.json'
-    NUM_TERMS_JSON = 'data/num_terms.json'
-    RAW_DESC_BASE = 'data/raw_descs/'
