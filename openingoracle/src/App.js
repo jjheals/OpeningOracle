a5bf76aeb31a59b991aacd20b1419eaab9efa0d7
zsarrett <zbsarrett@wpi.edu>
2023-11-20 16:23:27 -0500

    Working on styling

--- openingoracle/src/App.js
+++ openingoracle/src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import './MessageBox.css';
-import MessageBox from './MessageBox.js'
+import MessageBox from './MessageBox.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>OpeningOracle</p>
-        <MessageBox id = "InputBox"> </MessageBox>
-        
+        <p style={{color: "red"}}>OpeningOracle</p>
+        <MessageBox id="InputBox" />
       </header>
     </div>
   );
 }
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App /> 
+  </ThemeProvider>,
+  document.getElementById('root')
+);
 
 export default App;
--- openingoracle/src/MessageBox.js
+++ openingoracle/src/MessageBox.js
@@ -1,20 +1,44 @@
 import { useState } from "react";
-import './MessageBox.css';
+//import './MessageBox.css';
 import TextField from '@mui/material/TextField';
-import {styled} from '@mui/material/styles';
+//import {styled} from '@mui/material/styles';
+import { styled } from '@mui/system';
 
 
+
+//This changes the MessageBox. Color changes the text of the box label, not the input 
 const StyledTextField = styled(TextField)({
-"& label": {
-    color: "white"
-}
-})
+    '& .MuiInputLabel-root': {
+      color: 'red',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    }
+  });
+  
 
+
+// function MessageBox() {
+//     let [message, setMessage] = useState("Test message")
+
+//     return (<StyledTextField label="Type Here" />)
+// }
 
 function MessageBox() {
-    let [message, setMessage] = useState("Test message")
-
-    return (<StyledTextField label="Type Here" />)
-}
-
+    let [message, setMessage] = useState("Test message");
+  
+    return (
+      <TextField
+        label="Type Here"
+        InputLabelProps={{
+          sx: {
+            color: 'red',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          },
+        }}
+      />
+    );
+  }
 export default MessageBox;
