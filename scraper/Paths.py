cf3a196a97cbe2467fdfa7cd6dff0ea8d68179e9
Justin Healey <jjheals@gmail.com>
2023-11-25 16:46:16 -0500

    Revert "Merge"
    This reverts commit 484130554f8b8741a963058c0b9b8b213b6abe2d, reversing
    changes made to 49b8c51dc2bd39a3912e0c42a6a352b5094cf2f1.

---
+++ scraper/Paths.py
@@ -0,0 +1,6 @@
+class Paths: 
+    LICHESS_DATASET = 'data/kaggle-lichess.csv'
+    OPENINGS_JSON = 'data/openings.json'
+    INDEX_JSON = 'data/index.json'
+    NUM_TERMS_JSON = 'data/num_terms.json'
+    RAW_DESC_BASE = 'data/raw_descs/'
